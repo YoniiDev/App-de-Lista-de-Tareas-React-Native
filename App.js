1c76feacc1a7cd985c32f986ad4bd4ccd2641664
Yonathan Bravo <Yonathan.ybf@gmail.com>
2024-03-27 16:52:49 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, TouchableOpacity, Modal, Button, TextInput } from 'react-native';
+import { useState } from "react";
 export default function App() {
+
+  const [modalVisible, setModalVisible] = useState(false)
+
+  const handleOpenModal = () => {
+    setModalVisible(true)
+  }
+
+  const handleChangeText = () => {
+
+  }
+
+  const handleAddTask = () => {
+
+  }
+
+  const handleCancelModal = () => {
+    setModalVisible(false)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.headerContainer}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.titleText}>Lista de Tareas</Text>
+        </View>
+        <TouchableOpacity
+          style={styles.addTaskContainer}
+          onPress={handleOpenModal}>
+          <Text style={styles.addTaskText}>+</Text>
+        </TouchableOpacity>
+      </View>
+      <Modal
+        visible={modalVisible}
+        animationType="slide"
+        transparent={true}>
+        <View>
+          <View>
+            <Text>Ingrese una tarea</Text>
+          </View>
+        </View>
+        <View>
+          <TextInput
+            // style={styles}
+            onChangeText={handleChangeText}
+            // value={textItem}
+            placeholder={"Comenzemos..."}
+          />
+        </View>
+        <View>
+          <Button title="Añadir" onPress={handleAddTask} />
+          <Button title="Cancelar" onPress={handleCancelModal} />
+        </View>
+      </Modal>
+
+
     </View>
   );
 }
@@ -13,8 +63,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#CE0930',
+    paddingTop: 30,
+    paddingHorizontal: 10
   },
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  titleContainer: {
+
+  },
+  titleText: {
+    fontSize: 24,
+    color: '#fff',
+    fontWeight: 'bold'
+  },
+  addTaskContainer: {
+    backgroundColor: '#fff',
+    paddingHorizontal: 10,
+    borderRadius: 6,
+  },
+  addTaskText: {
+    fontSize: 24,
+    color: '#CE0930',
+  }
 });
