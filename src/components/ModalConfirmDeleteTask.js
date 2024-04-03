d6b4344695f2ca21801ba2b4b3569c297bacab5f
Yonathan Bravo <Yonathan.ybf@gmail.com>
2024-04-02 21:15:07 -0300

    se añadieron estilos al ModalConfirmDeleteTask

--- src/components/ModalConfirmDeleteTask.js
+++ src/components/ModalConfirmDeleteTask.js
@@ -9,15 +9,17 @@ const ModalConfirmDeleteTask = ({ modalConfirmDeleteTaskVisible, taskSelected, h
             animationType="slide"
             transparent={true}>
             <View style={styles.modalStyle}>
-                <View style={styles.textModalContainer}>
-                    <Text style={styles.textModal}>¿Esta seguro que desea eliminar?</Text>
-                </View>
-                <View>
-                    <Text>{taskSelected.value}</Text>
-                </View>
-                <View>
-                    <Button title="Eliminar" onPress={handleDeletedTask} />
-                    <Button title="Cancelar" onPress={handleCancelModalConfirmDeleteTask} />
+                <View style={styles.modalContainer}>
+                    <View style={styles.textModalContainer}>
+                        <Text style={styles.textModal}>¿Esta seguro que deseas eliminar?</Text>
+                    </View>
+                    <View style={styles.textModalContainer}>
+                        <Text style={styles.textModal}>{taskSelected.value}</Text>
+                    </View>
+                    <View style={styles.buttonModalContainer}>
+                        <Button title="Eliminar" onPress={handleDeletedTask} />
+                        <Button title="Cancelar" onPress={handleCancelModalConfirmDeleteTask} />
+                    </View>
                 </View>
             </View>
         </Modal>
@@ -27,5 +29,32 @@ const ModalConfirmDeleteTask = ({ modalConfirmDeleteTaskVisible, taskSelected, h
 export default ModalConfirmDeleteTask
 
 const styles = StyleSheet.create({
+    modalStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+        backgroundColor: "#cccccc90"
+    },
+    modalContainer: {
+        backgroundColor: "black",
+        width: "80%",
+        alignItems: 'center',
+        gap: 20,
+        paddingVertical: 20,
+        borderRadius: 10,
+        borderColor: "red",
+        borderWidth: 2
+    },
+    textModalContainer: {
 
+    },
+    textModal: {
+        color: "white",
+        fontWeight: "bold",
+        fontSize: 16
+    },
+    buttonModalContainer: {
+        flexDirection: 'row',
+        gap: 30
+    },
 })
